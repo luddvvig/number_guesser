0700bdd6fe1e0ef20a16036e053e4cb47e4c291c
Ludwig <luddwig97@gmail.com>
2025-02-09 13:55:33 +0100

    Med funktioner försökte göra själv

--- number_guesser.py
+++ number_guesser.py
@@ -1,55 +1,63 @@
 import random
-#Makes the range for guessing
-highest = int(input("\nHur högt vill du gissa?"))
-answer = random.randint(1, highest)
-#I want to make it so that a higher range for guessing gives more lives and higher score.
-score = 0
-guesses = 5
 
-#to keep the loop going
+def starter_input():
+    global playing_range, name
+    name = input("Enter player name: ")
+    print(f"Welcome {name}!")
+    playing_range = int(input("Enter the range you want to play in: "))
+    return playing_range, name
 
-final_score = 0
-final_guesses = 0
-print(f"Bra! Nu får du gissa på ett nummer mellan 1 och {highest}")
-
-#while is_true == True: keeps my loop going
-
-while True:
-    guess = int(input("Skriv din gissning här: "))
-    print(str(answer))
-    if guess == answer:
-        print("Rätt! Du får ett poäng")
-        score += 1
-        final_score += 1
-        final_guesses += 1
-        print(f"Dina poäng: {score}")
-        answer = random.randint(1, highest) # generates a new number when guessing right
-    elif guess != answer:
-        print("Fel! Du förlorade ett liv.")
-        guesses -= 1
-        final_guesses += 1
-        print(f"Liv kvar: {guesses}")
-    if score == 5:
-        print("Du vann spelet!")
-        again = input("Vill du spela igen? Y/N:")
-        if again.lower() == "y":
-            score = 0
-        elif again.lower() == "n": 
+def game(playing_range, name):
+    answer = int(random.randrange(1, playing_range))
+    global final_guesses, final_score
+    score = 0
+    guesses = 5
+    final_score = 0
+    final_guesses = 0
+    print(f"Great {name} Now you can guess a number between 1 and {playing_range}")
+    while True:
+        guess = int(input("Enter your guess here: "))
+        if guess == answer:
+            print("Correct! You get a point")
+            score += 1
+            final_score += 1
+            final_guesses += 1
+            print(f"{name}s score: {score}")
+            answer = random.randint(1, playing_range)
+        elif guess != answer:
+            print("Wrong! You lost a life.")
+            guesses -= 1
+            final_guesses += 1
+            print(f"{name}s guesses left: {guesses}")
+        if score == 5:
+            print("You won the game!")
+            if play_again() == True:
+                score = 0
+            else:
+                break
+        elif guesses == 0:
+            print("You lost!")
+            if play_again() == True:
+                guesses = 5
+            else:
+                break
+            
+def play_again():
+    while True:
+        again = input("Vill du spela igen? Y/N: ").lower()
+        if again == "y":
+            return True
+        elif again == "n":
             print("Kul att du spelade!")
-            break
+            print(f"Du fick totalt {final_score} poäng och gjorde {final_guesses} gissningar totalt")
+
+            return False
         else:
-            print("error")
-            break
-    elif guesses == 0:
-        print("Du förlorade!")
-        again = input("Vill du ha revansch? Y/N:")
-        if again.lower() == "y":
-            guesses = 5
-        elif again.lower() == "n": 
-            print("Kul att du spelade!")
-            break
-        else:
-            print("error")
-            break
-        
-print(f"Du fick totalt {score} poäng och gjorde {final_guesses} gissningar totalt")
+            print("Ogiltigt val, vänligen ange Y eller N.")
+
+def main():
+    starter_input()
+    game(playing_range, name)
+
+if __name__ == "__main__":
+    main()
